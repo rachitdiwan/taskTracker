ec6fe0beb248060cf524de19f45c4505b36e0b04
rachitdiwan <rachitdiwan17@gmail.com>
2021-07-04 15:43:03 +0530

    final touch

--- app.js
+++ app.js
@@ -109,6 +109,11 @@ app.get("/:section", function(req,res){
   })
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
